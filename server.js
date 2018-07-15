286726222c2790fcdc0c3e8df5acf373dcce417e
Mau Lomba <mauriciolombard@liamdeMacBook-Air.local>
2018-07-15 07:33:04 -0500

    lecture 217 Storing user passwords. bcrypt-nodejs

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser'); // to parse the JSON
+const bcrypt = require('bcrypt-nodejs');
 
 const app = express();
 
@@ -11,7 +12,6 @@ const database = {
             id: "123",
             name: "John",
             email: "john@gmail.com",
-            password: "cookies",
             entries: 0,
             joined: new Date()
         },
@@ -19,13 +19,17 @@ const database = {
             id: "124",
             name: "Sally",
             email: "sally@gmail.com",
-            password: "bananas",
             entries: 0,
             joined: new Date()
         },
+    ],
 
-
-
+    login: [
+        {
+            id: '987',
+            hash: '',
+            email: 'john@gmail.com'
+        }
     ]
 }
 
@@ -34,6 +38,14 @@ app.get('/', (req, res)=>{
 });
 
 app.post('/signin', (req, res) => {
+        // Load hash from your password DB.
+    bcrypt.compare("soupy", '$2a$10$2oeu/EhZSVUbqA099cRAKum1m5SY0qNKb3JsDrPRKmVMuC4jsvvLG', function(err, res) {
+        console.log('first guess', res);
+    });
+    bcrypt.compare("veggies", '$2a$10$2oeu/EhZSVUbqA099cRAKum1m5SY0qNKb3JsDrPRKmVMuC4jsvvLG', function(err, res) {
+        console.log('second guess', res);
+    });
+
     if(req.body.name === database.users[0].name && req.body.password === database.users[0].password){
         res.json('success');
     } else {
@@ -42,7 +54,11 @@ app.post('/signin', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
+
     const {name, email, password} = req.body;
+    bcrypt.hash(password, null, null, function(err, hash) {
+    console.log(hash);
+    });
 
     database.users.push({
         id: "126",
